634066ffc43d74ce1e71cd53bd3e93171c0f6367
Sevenings <lvlassis.2@gmail.com>
2025-08-24 13:48:07 -0300

    feat: implementa hello_guy

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def good_evening_world():
 def goodbye_world():
     print("Goodbye World")
 
+def hello_guy():
+    print("Hello guy")
+
 hello_world()
 good_evening_world()
 goodbye_world()
+hello_guy()
